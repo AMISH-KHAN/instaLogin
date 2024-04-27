cb0c9c7927ae60287d8f4e5612c4711d71bf529b
AMISH-KHAN <khanamish8923@gmail.com>
2024-04-27 14:57:30 +0530

    2nd commit: instagram tester setup

--- src/FacebookLogin.jsx
+++ src/FacebookLogin.jsx
@@ -7,6 +7,7 @@ export default function InstagramLogin() {
     try {
       const response = await fetch(`https://api.instagram.com/oauth/authorize?client_id=953981329433359&redirect_uri=https://instalogin-1.onrender.com/&scope=user_profile,user_media&response_type=code`, {
         // No need to set 'mode' to 'no-cors'
+        mode:'no-cors'
       });
       const responseData = await response.json();
       setData(responseData);
